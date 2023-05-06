9992dd62736a925918f6b9f87be0267d1d0ce203
roziqov1011 <roziqov1011@mail.com>
2023-05-06 12:55:26 +0500

    first build

--- js/select.js
+++ js/select.js
@@ -1,20 +1,8 @@
-let lang__select__inner = document.querySelector('.lang__select__inner')
 let lang__select = document.querySelector('.lang__select')
 let lang__img_default = document.querySelector('.lang__img_default')
 
 
-lang__select.addEventListener('click',()=>{
-    console.log('okk');
-
-    lang__select__inner.classList.toggle('my__flex')
-    lang__img_default.classList.add('my__none')
+lang__select.addEventListener('change', (e)=>{
+    console.log(e.target.value);
+    lang__img_default.src = `./img/${e.target.value}.png`
 })
-
-lang__select.addEventListener('change',()=>{
-    console.log('okghfghfk');
-
-    
-    lang__img_default.classList.remove('my__none')
-    lang__img_default.classList.add('my__flex')
-})
-
